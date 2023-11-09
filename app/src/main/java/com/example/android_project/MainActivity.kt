9057a3289c8106b61f9567a2399523c26b76ee39
suminHan1104 <hansumin1104@gmail.com>
2023-11-09 17:30:31 +0900

    기본 로그인 설정

--- app/src/main/java/com/example/android_project/MainActivity.kt
+++ app/src/main/java/com/example/android_project/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.android_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        Firebase.auth.signInWithEmailAndPassword("1111@gmail.com", "111111")
+            .addOnCompleteListener(this) { // it: Task<AuthResult!>
+                if (it.isSuccessful) {
+                    textView.text = "sign-in success ${Firebase.auth.currentUser?.uid}"
+
+                } else {
+                    textView.text = "sign-in failed"
+                }
+            }
     }
 }
