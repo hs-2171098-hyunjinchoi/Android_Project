837a19d1706376d1bede1f05c99a2f7a32f23616
Seungbin <2171e91@hansung.ac.kr>
2023-11-10 17:49:21 +0900

    로그인 뼈대 구성2

--- app/src/main/java/com/example/android_project/MainActivity.kt
+++ app/src/main/java/com/example/android_project/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android_project
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import com.example.android_project.databinding.ActivityMainBinding
 import com.google.firebase.auth.ktx.auth
@@ -16,25 +17,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         // When you first run the app, there is no user,
         // so you switch to LoginActivity immediately.
-        println("체크")
-        if(Firebase.auth.currentUser==null){
+        if(Firebase.auth.currentUser == null){
             startActivity(
                 Intent(this, LoginActivity::class.java)
             )
             finish()
         }
-
-//        setContentView(R.layout.activity_main)
-//
-//        val textView = findViewById<TextView>(R.id.textView)
-//        Firebase.auth.signInWithEmailAndPassword("1111@gmail.com", "111111")
-//            .addOnCompleteListener(this) { // it: Task<AuthResult!>
-//                if (it.isSuccessful) {
-//                    textView.text = "sign-in success ${Firebase.auth.currentUser?.uid}"
-//
-//                } else {
-//                    textView.text = "sign-in failed"
-//                }
-//            }
     }
 }
